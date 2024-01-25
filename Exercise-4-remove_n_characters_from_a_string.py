5aa2f9d4a405c35c7f53d35f77318f6164f5392b
unknown <miggy.payumo26@gmail.com>
2024-01-26 03:48:00 +0800

    Pseudocode and code can now accept input and print it

--- Exercise-4-remove_n_characters_from_a_string.py
+++ Exercise-4-remove_n_characters_from_a_string.py
@@ -1,1 +1,10 @@
+
 # Pseudocode
+# 1. Ask a user to input a string 
+# 2. Analyze the string given by the user
+# 3. Ask the user for a number less than the amount of characters of the string given
+# 4. Print the string but remove the first n characters, where n is the given value of the user
+
+while True:
+    character_input = input("Please type a word: ")
+        
